6043188f2fef4001a06402b8d33b2a97be66996a
Arni-b24ee1021 <b24ee1021@iitj.ac.in>
2025-04-14 01:21:20 +0530

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -43,7 +43,7 @@ int mtictactoe() {
     int player;
     int winner = 0;
 
-    // Initialize 3x3 board with empty spaces
+    // make the board
      for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             boardT[i][j]=' ';
