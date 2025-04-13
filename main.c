a8e2712f474a146cd195088aba4137f380dbb0bc
aditya8281 <b24ee1062@iitj.ac.in>
2025-04-13 15:20:56 +0530

    Updated

--- main.c
+++ main.c
@@ -3,44 +3,14 @@
 
 #include "header.h"
 
+void game();
+
 int main()
 {
-    
-    printf("Which game you want to play...\n");
-    printf("1. 2048\n2. Mine Sweeper\n3. tictactoe\n");
-    printf("\nEnter B to Go back to previous page\nEnter E to Exit\n");
-
-
-
-    char n;
-    scanf("%c",&n);
-
-    if(n=='1')
-    {
-        m2048();
-
-    }
-    else if(n=='2')
-    {
-        mMineSweeper();
-    }
-    else if(n=='3')
-    {
-        mtictactoe();
-    }
-    else if(n=='B'||n=='b')
-    {
-
-    }
-    else if(n=='E'||'e')
-    {
-        exit(0);
-    }
-    else
-    {
-        printf("Invalid Input!!!");
-    }
+    game();
     return 0;
-
     
 }
+
+
+
